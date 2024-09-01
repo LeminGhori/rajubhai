7733b16539cf014922de4f8f98fb0a030e5c334f
LeminGhori <ghorilemin@gmail.com>
2024-09-01 09:20:18 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from "./pages/home/Home.jsx";
+
+function App() {
+  return (
+    <div className="text-center p-4">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
